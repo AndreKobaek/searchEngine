8fb68b7c28cc60f340fad367e88e5e66dba1a09f
Mikkel Smedemand <mbsm@itu.dk>
2018-12-02 11:18:45 +0100

    Add documentation to QueryHandler.java.

--- src/main/java/searchengine/QueryHandler.java
+++ src/main/java/searchengine/QueryHandler.java
@@ -6,14 +6,11 @@ import java.util.Set;
 /**
  * This class is responsible for answering queries to our search engine.
  */
-
 public class QueryHandler {
 
   /** The index the QueryHandler uses for answering queries. */
   private Index idx = null;
 
-//  /** The corpus */
-//  private Corpus corpus = null;
 
   /**
    * The constructor
@@ -24,15 +21,6 @@ public class QueryHandler {
     this.idx = idx;
   }
 
-//  /**
-//   * Another constructor
-//   * 
-//   * @param idx The index used by the QueryHandler.
-//   */
-//  public QueryHandler(Index idx, Corpus corpus) {
-//    this.idx = idx;
-//    this.corpus = corpus;
-//  }
 
   /**
    * getMachingWebsites answers queries of the type "subquery1 OR subquery2 OR subquery3 ...". A
@@ -43,21 +31,36 @@ public class QueryHandler {
    * @return the set of websites that matches the query
    */
   public Set<Website> getMatchingWebsites(String query) {
+    // The set is initialized as a HashSet rather than a TreeMap,
+    // since the set of websites that matches a query is expected to be fairly small,
+    // most of the time.
     Set<Website> results = new HashSet<>();
 
+    // Split query into subquerys separated by OR.
     String[] subquerys = query.split("\\sOR\\s");
+
+    // find sets that matches each subquery and add them.
     for (int j = 0; j < subquerys.length; j++) {
       String[] words = subquerys[j].split("\\s");
-      results.addAll(intersect(words));
+      results.addAll(intersect(words)); // lookup all the words in the subquery and take
+                                        // intersection.
+                                        // then take union of sets from each subquery.
     }
     return results;
   }
 
+
+
   private Set<Website> intersect(String[] words) {
+    // The set is initialized as a HashSet rather than a TreeMap,
+    // since the set of websites that matches a single word is expected to be fairly small,
+    // most of the time.
     Set<Website> results = new HashSet<>();
 
-    // intersection of sets of websites containing the words
-    results.addAll(idx.lookup(words[0]));
+    // intersection of sets of websites containing at least one of the words in the query.
+    // this is where the lookup in the index happens.
+    results.addAll(idx.lookup(words[0])); // the first set added should preferable be the biggest
+                                          // set, because...???
     for (int i = 1; i < words.length; i++) {
       results.retainAll(idx.lookup(words[i]));
     }
