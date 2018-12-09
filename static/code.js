ab4826f70545a38325f71770c8c8d65a8c13ca13
Flemming Westberg <flso@itu.dk>
2018-12-09 19:55:31 +0100

    Search on enter

--- static/code.js
+++ static/code.js
@@ -1,27 +1,37 @@
 
-$(document).ready(function() {
+// Once the DOM is ready, attach event listeners
+$(document).ready(function () {
+    // Call lookup() on click
+    $("#searchbutton").click(function () {
+        lookup();
+    });
+    
+    // Call lookup() on enter, if focus is in #searchbox
+    $("#searchbox").keypress(function(e) {
+        if(e.which == 13)
+            lookup();
+      });
+});
+
+// Look up the search query and write the results 
+function lookup() {
+    console.log("Sending request to server.");
     var baseUrl = "http://localhost:8080";
 
-    $("#searchbutton").click(function() {
-        console.log("Sending request to server.");
-        $.ajax({
-            method: "GET",
-            url: baseUrl + "/search",
-            data: {query: $('#searchbox').val()}
-        }).success( function (data) { 
-            console.log("Received response " + data);
-            $("#responsesize").html("<p>" + data.length + " websites retrieved</p>");
-            // var buffer = "<ul>\n";
-            // $.each(data, function(index, value) { 
-            //     buffer += "<li><a href=\"" + value.url + "\" target=\"_blank\">" + value.title + "</a></li>\n";
-            // });
-            // buffer += "</ul>";
-            var buffer = "";
-            $.each(data, function(index, value) { 
-                var preview = JSON.stringify(value.words).replace(/,/g, " ");
-                buffer += "<div>\n<a href=\"" + value.url + "\" target=\"_blank\">" + value.title + "</a>\n<p>" + preview + "</p></div>\n";
-            });
-            $("#urllist").html(buffer);
+    $.ajax({
+        method: "GET",
+        url: baseUrl + "/search",
+        data: { query: $('#searchbox').val() }
+    }).success(function (data) {
+        console.log("Received response " + data);
+        $("#responsesize").html("<p>" + data.length + " websites retrieved</p>");
+        var buffer = "";
+        
+        $.each(data, function (index, value) {
+            var preview = JSON.stringify(value.words).replace(/,/g, " ");
+            buffer += "<div>\n<a href=\"" + value.url + "\" target=\"_blank\">" + value.title + "</a>\n<p>" + preview + "</p></div>\n";
         });
+
+        $("#urllist").html(buffer);
     });
-});
+}
