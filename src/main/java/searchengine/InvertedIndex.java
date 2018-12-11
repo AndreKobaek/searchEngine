f3eed85dcffaca48fdb05f820705164651499cd6
Flemming Westberg <flso@itu.dk>
2018-12-11 17:42:21 +0100

    One more time

--- src/main/java/searchengine/InvertedIndex.java
+++ src/main/java/searchengine/InvertedIndex.java
@@ -6,8 +6,6 @@ import java.util.Set;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
 
 
 /**
@@ -53,6 +51,7 @@ public abstract class InvertedIndex implements Index {
       }
     }
   }
+
   
   
   /**
