b5b084512dc85a691b1ef0e18995d229499bac67
Mikkel Smedemand <mbsm@itu.dk>
2018-12-14 02:14:04 +0100

    Add documentation to Fuzzy. Also fix what appeared like a small mistake in calculate2gram.

--- src/main/java/searchengine/Fuzzy.java
+++ src/main/java/searchengine/Fuzzy.java
@@ -13,6 +13,14 @@ public class Fuzzy {
     this.corpus = corpus;
   }
 
+  /**
+   * If a word doesn't appear in the corpus, the expand method tries to find other
+   * words in the corpus that are close to it. As a measure of the distance between words,
+   * we use the Levenshtein distance.   
+   * 
+   * @param unknownWord
+   * @return a set of strings with words that are close to the mispelled word.
+   */
   public Set<String> expand(String unknownWord) {
 
     // Set for storing the fuzzy strings
@@ -20,6 +28,7 @@ public class Fuzzy {
 
     // maximum allowed edit distance.
     int delta;
+    
     // delta is assigned based on the length of the word
     switch (unknownWord.length()) {
       case 3:
@@ -38,36 +47,40 @@ public class Fuzzy {
 
     // only looking at 2-grams for now
     int gramSize = 2;
-
+    
+    // number of unique words in the corpus
     int ncols = corpus.getWordCountUnique();
 
     Set<String> approximateStrings = new HashSet<>();
 
+    // the summedRowVector keeps track of how many grams each 
+    // word in the corpus has in common with the unknownWord.
     int[] summedRowVector = new int[ncols];
+    
+    // go through all bigrams for unknownWord
     for (String bigram : calculate2Gram(unknownWord)) {
+      
+      // get "boolean" vector of all the words containing the bigram.
+      int[] rowVector = corpus.getBiGramMap().get(bigram);
       for (int ncol = 0; ncol < ncols; ncol++) {
-        // HER
-        int[] rowVector = corpus.getBiGramMap().get(bigram);
+        // add vector for bigram to the summed vector. 
         summedRowVector[ncol] += rowVector[ncol];
       }
     }
 
     // add approximate words
     for (int i = 0; i < summedRowVector.length; i++) {
-      // HER
+      // words that have too few grams in common with unknownWord, are ignored.
       int commonGramsBound =
           Math.max(unknownWord.length(), corpus.getWordsInCorpus().get(i).length()) - 1
               - (delta - 1) * gramSize;
       if (summedRowVector[i] >= commonGramsBound) {
-        // HER
+        // words that have sufficient grams, might be within allowed edit distance. 
         approximateStrings.add(corpus.getWordsInCorpus().get(i));
       }
     }
-    // print message
-    System.out.println("Other related words based on 2-gram index: ");
-    System.out.println(approximateStrings.toString());
 
-    // pick the best of the approximate strings, the one(s) with the smallest edit distance.
+    // only keep the approximate strings with edit distance less or equal to allowed.
     for (String approxString : approximateStrings) {
 
       // check if editDistance is smaller than delta
@@ -75,20 +88,26 @@ public class Fuzzy {
       if (editDistance <= delta) {
         fuzzyStrings.add(approxString);
       } else {
-        System.out.println(
-            "Discard: " + approxString + " (Edit Distance = " + editDistance + " > " + delta + ")");
+         // do nothing
       }
     }
-    System.out.println("I'll try to search for:");
+    System.out.println("Cannot find word: " + unknownWord + " Instead I'll try to search for:");
     System.out.println(fuzzyStrings.toString());
-
     return fuzzyStrings;
   }
 
 
   /**
-   * Calculate edit distance for two strings x and y. algorithm from reference: "The
+   * Calculate edit distance for two strings x and y. Algorithm from reference: "The
    * String-to-string correction problem", R. A. Wagner and M. J. Fischer
+   * 
+   * Allowed edit operations are: delete, insert, change. 
+   * The cost for all edit operations are chosen equal to 1.  
+   * 
+   * @param word x
+   * @param other word y
+   * 
+   * @return the edit distance between 
    */
   private int editDistance(String x, String y) {
 
@@ -120,26 +139,37 @@ public class Fuzzy {
           equalIndicator = 0;
         }
 
-        // do something
+        // calculate m1, m2 and m3 and put the minimum value into the matrix element Dij. 
         int m1 = D[i - 1][j - 1] + equalIndicator;
         int m2 = D[i - 1][j] + deleteCost;
         int m3 = D[i][j - 1] + insertCost;
         D[i][j] = Math.min(m1, Math.min(m2, m3));
       }
     }
+    // return the value from the lower right corner of the D-matrix.
     return D[x.length()][y.length()];
   }
 
 
   /**
    * Calculate 2-grams for a word.
+   * 
+   * @param word for which 2-grams must be calculated.
+   * @return set of 2-grams that the word contains, including "$s1", and "$sn" where
+   * s1 is the first letter, and sn is the last letter in the word.  
    */
   private Set<String> calculate2Gram(String word) {
 
     Set<String> biGrams = new HashSet<>();
 
-    // If the word has <= 1 character, return an empty set
-    if (word.length() <= 1) {
+    // If the word has < 1 character, return an empty set
+    if (word.length() < 1) {
+      return biGrams;
+    }
+    
+    // If the word has == 1 character
+    if (word.length() == 1) {
+      biGrams.add("$"+word);
       return biGrams;
     }
 
